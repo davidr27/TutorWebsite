63286ccc117a626c97463e4ef6e9bbe22847d540
David Ramirez <david.9706@hotmail.com>
2021-12-23 19:18:51 -0800

    Routing created for About Us

--- tutorWeb/src/app/app-routing.module.ts
+++ tutorWeb/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent} from "./about-us/about-us.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'aboutUs', component: AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
