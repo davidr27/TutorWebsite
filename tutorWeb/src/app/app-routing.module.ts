6d662fc56e04f695313a980af691d35ecb4dbe9e
David Ramirez <david.9706@hotmail.com>
2021-12-23 19:58:22 -0800

    Update routing

--- tutorWeb/src/app/app-routing.module.ts
+++ tutorWeb/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent} from "./about-us/about-us.component";
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-  {path: 'aboutUs', component: AboutUsComponent}
+  {path: 'aboutUs', component: AboutUsComponent},
+  {path: 'app', component: AppComponent},
+  {path: '', redirectTo: 'app', pathMatch: 'full'}
 ];
 
 @NgModule({
