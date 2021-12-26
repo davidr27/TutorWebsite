f13ee3ea37dc46e903ab7c0d6acaeb1f8beaec9c
David Ramirez <david.9706@hotmail.com>
2021-12-25 23:41:03 -0800

    Interface Created for Menu Images

--- tutorWeb/src/app/app.component.ts
+++ tutorWeb/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//importing api-object interface menu images
+import { ImagesMenu} from "./api-objects/imagesMenu";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tutorWeb';
+
+  images: ImagesMenu = {images: ['assets/images/latinoYouth.png', 'assets/images/newyork.png']};
+  constructor() {
+
+  }
+
+  ngOnInit(){
+    console.log('OnInit was called');
+  }
+
 }
